f7c9fce89f5d1e996b07366c683fd596cf871e21
Roger Lipscombe <roger@differentpla.net>
2022-12-01 17:58:27 +0000

    Day 1, Rust

--- rust/day01/src/main.rs
+++ rust/day01/src/main.rs
@@ -1,3 +1,14 @@
+use std::fs::read_to_string;
+
 fn main() {
-    println!("Hello, world!");
+    let input = read_to_string("input.txt").unwrap();
+    let elves = input
+        .trim_end()
+        .split("\n\n")
+        .map(|s| s.split("\n").map(|x| x.parse::<i32>().unwrap()));
+
+    let mut sums: Vec<i32> = elves.map(|elf| elf.sum()).collect();
+    sums.sort_by(|a, b| b.cmp(a));
+    println!("{}", sums[0]);
+    println!("{}", sums[0] + sums[1] + sums[2]);
 }
