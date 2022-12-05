9d2df3945cd49c139e5beb75fa7d433c68ea8355
Roger Lipscombe <roger@differentpla.net>
2022-12-05 20:42:01 +0000

    Day 5, part 2, Python

--- python/day05.py
+++ python/day05.py
@@ -14,7 +14,7 @@ with open(input_file) as f:
     [init, insts] = f.read().split("\n\n")
     for line in init.split("\n"):
         line = line.strip("\n")
-        print(line)
+        # print(line)
 
         for stack in range(stack_count):
             col = 4 * stack + 1
@@ -29,18 +29,24 @@ with open(input_file) as f:
         if m:
             instructions.append(m.groups())
 
-print(stacks)
+# print(stacks)
 
-def move(stacks, count, src, dst):
+def move9000(stacks, count, src, dst):
     for _ in range(count):
         crate = stacks[src - 1].pop()
         stacks[dst - 1].append(crate)
     pass
 
-for inst in instructions:
-    move(stacks, int(inst[0]), int(inst[1]), int(inst[2]))
+def move9001(stacks, count, src, dst):
+    split = len(stacks[src - 1]) - count
+    temp = stacks[src - 1][split:]
+    stacks[src - 1] = stacks[src - 1][:split]
+    stacks[dst - 1].extend(temp)
 
-print(stacks)
+for inst in instructions:
+    move9001(stacks, int(inst[0]), int(inst[1]), int(inst[2]))
+
+# print(stacks)
 
 result = ""
 for stack in stacks:
