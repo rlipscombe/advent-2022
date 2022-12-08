7a9e0fc020454b684c4ab166a424d7ddb5d94759
Roger Lipscombe <roger@differentpla.net>
2022-12-08 20:57:29 +0000

    Use a script

--- erlang/run.sh
+++ erlang/run.sh
@@ -1,23 +1,17 @@
-```
+#!/usr/bin/env bash
+
 ./day06.escript 4 'mjqjpqmgbljsphdztnvjfqwrcgsmlb'	 # 7
 ./day06.escript 4 'bvwbjplbgvbhsrlpgdmjqwftvncz'	 # 5
 ./day06.escript 4 'nppdvjthqldpwncqszvftbrmjlhg'	 # 6
 ./day06.escript 4 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg' 	 # 10
 ./day06.escript 4 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'	 # 11
-```
 
-```
 ./day06.escript 4 "$(<input06.txt)"			 # ???
-```
 
-```
 ./day06.escript 14 'mjqjpqmgbljsphdztnvjfqwrcgsmlb'	 # 19
 ./day06.escript 14 'bvwbjplbgvbhsrlpgdmjqwftvncz'	 # 23
 ./day06.escript 14 'nppdvjthqldpwncqszvftbrmjlhg' 	 # 23
 ./day06.escript 14 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg' 	 # 29
 ./day06.escript 14 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'	 # 26
-```
 
-```
 ./day06.escript 14 "$(<input06.txt)"			 # ???
-```
