272b0a06b0ef03887f301785dcb61b83f21e1ab3
Roger Lipscombe <roger@differentpla.net>
2022-12-11 11:50:13 +0000

    Create new dart project

---
+++ dart/day11/bin/day11.dart
@@ -0,0 +1,5 @@
+import 'package:day11/day11.dart' as day11;
+
+void main(List<String> arguments) {
+  print('Hello world: ${day11.calculate()}!');
+}
---
+++ dart/day11/test/day11_test.dart
@@ -0,0 +1,8 @@
+import 'package:day11/day11.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
