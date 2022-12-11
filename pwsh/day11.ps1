eca3a698dabf754ff990cac218f78d9790d3ed36
Roger Lipscombe <roger@differentpla.net>
2022-12-11 20:30:13 +0000

    Day 11, part 1, Powershell

--- pwsh/day11.ps1
+++ pwsh/day11.ps1
@@ -1,4 +1,8 @@
-$path = [System.IO.Path]::Combine($PWD, "..\example11.txt")
+$ErrorActionPreference = 'Stop'
+
+$path = [System.IO.Path]::Combine($PWD, "..\input11.txt")
+$rounds = 20
+
 $lines = [System.IO.File]::ReadAllLines($path)
 
 # Parser
@@ -12,33 +16,37 @@ $lines = [System.IO.File]::ReadAllLines($path)
 $monkeys = @()
 
 $monkey = @{}
+$monkey.Inspections = 0
+$monkey.Items = [System.Collections.ArrayList](@())
+
 foreach ($line in $lines) {
     if ($line -match 'Monkey (\d+):') {
         $monkey.Id = $Matches.1
     }
     elseif ($line -match 'Starting items: (.*)') {
         $items = $Matches.1
-        $monkey.Items = $items.Split(", ")
+        $items = @($items.Split(", ") | ForEach-Object { [int]$_ })
+        $monkey.Items.AddRange($items)
     }
     elseif ($line -match 'Operation: new = old \* old') {
         $monkey.Operation = {
-            param { $worry }
-            Write-Output $worry * $worry
+            param($worry)
+            Write-Output ($worry * $worry)
         }
     }
     elseif ($line -match 'Operation: new = old \+ (\d+)') {
         $value = $Matches.1
         $monkey.Operation = {
-            param { $worry }
-            Write-Output $worry + $value
-         }.GetNewClosure()
+            param($worry)
+            Write-Output ($worry + $value)
+        }.GetNewClosure()
     }
     elseif ($line -match 'Operation: new = old \* (\d+)') {
         $value = $Matches.1
         $monkey.Operation = {
-            param { $worry }
-            Write-Output $worry * $value
-         }.GetNewClosure()
+            param($worry)
+            Write-Output ($worry * $value)
+        }.GetNewClosure()
     }
     elseif ($line -match 'Test: divisible by (\d+)') {
         $monkey.Divisor = $Matches.1
@@ -52,6 +60,8 @@ foreach ($line in $lines) {
     elseif ($line.Length -eq 0) {
         $monkeys += , $monkey
         $monkey = @{}
+        $monkey.Inspections = 0
+        $monkey.Items = [System.Collections.ArrayList](@())
     }
     else {
         throw $line
@@ -59,4 +69,43 @@ foreach ($line in $lines) {
 }
 
 $monkeys += , $monkey
-Write-Output $monkeys
+# Write-Output $monkeys
+
+for ($round = 1; $round -le $rounds; $round++) {
+    foreach ($monkey in $monkeys) {
+        while ($monkey.Items.Length) {
+            $item = $monkey.Items[0]
+            $monkey.Items.RemoveAt(0)
+
+            # Write-Output ("Monkey inspects an item with a worry level of {0}." -f $item)
+            $monkey.Inspections++
+            $item = &$monkey.Operation $item
+            # Write-Output ("...to {0}" -f $item)
+
+            $item = [int][Math]::Floor($item / 3)
+            # Write-Output ("...to {0}" -f $item)
+
+            $remainder = ($item % $monkey.Divisor)
+
+            if ($remainder -eq 0) {
+                # Write-Output ("true -> {0}" -f $monkey.IfTrue)
+                $monkeys[$monkey.IfTrue].Items.Add($item) | Out-Null
+            }
+            else {
+                # Write-Output ("false -> {0}" -f $monkey.IfFalse)
+                $monkeys[$monkey.IfFalse].Items.Add($item) | Out-Null
+            }
+        }
+    }
+
+    # foreach ($monkey in $monkeys) {
+    #     Write-Output ("{0}: {1}" -f $monkey.Id, ($monkey.Items -join ", "))
+    # }
+}
+
+# foreach ($monkey in $sorted) {
+#     Write-Output ("{0}: {1}" -f $monkey.Id, $monkey.Inspections)
+# }
+
+$sorted = $monkeys | Sort-Object Inspections -Descending
+Write-Output ($sorted[0].Inspections * $sorted[1].Inspections)
