ef066644a22a389f792b4da7ad823da335adb2e0
Roger Lipscombe <roger@differentpla.net>
2022-12-13 18:25:20 +0000

    wip

--- cxx/day12/day12.cpp
+++ cxx/day12/day12.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/dijkstra_shortest_paths.hpp>
 #include <boost/graph/graphviz.hpp>
 
 int main(int argc, char *argv[])
@@ -23,21 +24,94 @@ int main(int argc, char *argv[])
         lines.push_back(line);
     }
 
-    struct Vertex
-    {
-        int x;
-        int y;
-    };
-    typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, Vertex, boost::no_property> Graph;
+    typedef boost::adjacency_list<
+        boost::vecS, boost::vecS, boost::directedS,
+        std::tuple<int, int>, boost::property<boost::edge_weight_t, int>>
+        Graph;
 
     Graph g;
-    Vertex v1{0, 0};
-    Vertex v2{0, 1};
-    auto d1 = boost::add_vertex(v1, g);
-    auto d2 = boost::add_vertex(v2, g);
-    boost::add_edge(d1, d2, g);
 
-    boost::write_graphviz(std::cout, g);
+    // For each (x, y) in the input, add a vertex to the graph, and an edge to the
+    // appropriate neighbours.
+    int x_end = std::size(lines[0]);
+    int y_end = std::size(lines);
+    std::map<std::tuple<int, int>, Graph::vertex_descriptor> descriptors;
+
+    for (int x = 0; x < x_end; ++x)
+    {
+        for (int y = 0; y < y_end; ++y)
+        {
+            auto vertex = std::make_tuple(x, y);
+            auto v_desc = boost::add_vertex(vertex, g);
+            descriptors[vertex] = v_desc;
+        }
+    }
+
+    std::tuple<int, int> start;
+    Graph::vertex_descriptor start_desc;
+    std::tuple<int, int> end;
+    Graph::vertex_descriptor end_desc;
+    for (int x = 0; x < x_end; ++x)
+    {
+        for (int y = 0; y < y_end; ++y)
+        {
+            auto v_desc = descriptors[std::make_tuple(x, y)];
+            char height = lines[y][x];
+            if (height == 'S')
+            {
+                height = 'a';
+                start = std::make_tuple(x, y);
+                start_desc = v_desc;
+            }
+            if (height == 'E')
+            {
+                height = 'z';
+                end = std::make_tuple(x, y);
+                end_desc = v_desc;
+            }
+
+            // std::cout << "(" << x << ", " << y << ") is " << height << std::endl;
+
+            // NESW
+            if (y - 1 >= 0 && lines[y - 1][x] <= height + 1)
+            {
+                auto n_desc = descriptors[std::make_tuple(x, y - 1)];
+                boost::add_edge(v_desc, n_desc, g);
+            }
+            if (x + 1 < x_end && lines[y][x + 1] <= height + 1)
+            {
+                auto n_desc = descriptors[std::make_tuple(x + 1, y)];
+                boost::add_edge(v_desc, n_desc, g);
+            }
+            if (y + 1 < y_end && lines[y + 1][x] <= height + 1)
+            {
+                auto n_desc = descriptors[std::make_tuple(x, y + 1)];
+                boost::add_edge(v_desc, n_desc, g);
+            }
+            if (x - 1 >= 0 && lines[y][x - 1] <= height + 1)
+            {
+                auto n_desc = descriptors[std::make_tuple(x - 1, y)];
+                boost::add_edge(v_desc, n_desc, g);
+            }
+        }
+    }
+
+    // boost::write_graphviz(std::cout, g);
+
+    // Find paths from end back to start.
+    std::vector<Graph::vertex_descriptor> p(num_vertices(g));
+    std::vector<int> d(num_vertices(g));
+
+    boost::dijkstra_shortest_paths(
+        g, end_desc,
+        boost::predecessor_map(boost::make_iterator_property_map(p.begin(), get(boost::vertex_index, g)))
+            .distance_map(boost::make_iterator_property_map(d.begin(), get(boost::vertex_index, g))));
+
+    for (auto it = d.begin(); it != d.end(); ++it)
+    {
+        std::cout << *it << std::endl;
+    }
+    // std::cout << d[end_desc] << std::endl;
 
     return 0;
 }
