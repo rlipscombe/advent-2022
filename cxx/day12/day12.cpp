305bf32bb1ebebb95a20c2ffa7ec41686b5793bb
Roger Lipscombe <roger@differentpla.net>
2022-12-13 19:38:02 +0000

    lose logging

--- cxx/day12/day12.cpp
+++ cxx/day12/day12.cpp
@@ -20,9 +20,7 @@ using VertexDescriptor = boost::graph_traits<Graph>::vertex_descriptor;
 int height_of(Point pt, Lines lines)
 {
     auto [x, y] = pt;
-    std::cerr << "height_of " << x << " " << y << std::endl;
     char ch = lines[y][x];
-    std::cerr << "..." << ch << std::endl;
     if (ch == 'S')
     {
         return 'a';
@@ -50,7 +48,6 @@ Graph::vertex_descriptor vertex_descriptor_for(Point pt, int x_end, int y_end)
 bool is_in_bounds(Point pt, int x_end, int y_end)
 {
     auto [x, y] = pt;
-    std::cerr << "is_in_bounds " << x << ", " << y << std::endl;
     return x >= 0 && x < x_end && y >= 0 && y < y_end;
 }
 
@@ -58,7 +55,6 @@ void maybe_add_edge(Point from, Point to, Lines lines, int x_end, int y_end, Gra
 {
     if (is_in_bounds(to, x_end, y_end) && height_of(to, lines) <= height_of(from, lines) + 1)
     {
-        std::cerr << "adding edge from " << vertex_descriptor_for(from, x_end, y_end) << " to " << vertex_descriptor_for(to, x_end, y_end) << std::endl;
         add_edge(vertex_descriptor_for(from, x_end, y_end), vertex_descriptor_for(to, x_end, y_end), g);
     }
 }
@@ -80,8 +76,7 @@ Graph create_graph(std::vector<std::string> lines)
         for (int x = 0; x < x_end; ++x)
         {
             auto vertex = std::make_tuple(x, y);
-            auto vd = boost::add_vertex(vertex, g);
-            std::cerr << "(" << x << ", " << y << ") has descriptor " << vd << std::endl;
+            boost::add_vertex(vertex, g);
         }
     }
 
@@ -99,44 +94,8 @@ Graph create_graph(std::vector<std::string> lines)
         }
     }
 
-    std::cerr << "created graph" << std::endl;
     return g;
 }
-
-// Graph g;
-
-// std::map<std::tuple<int, int>, Graph::vertex_descriptor> descriptors;
-
-// std::tuple<int, int> start;
-// Graph::vertex_descriptor start_desc;
-// std::tuple<int, int> end;
-// Graph::vertex_descriptor end_desc;
-// for (int x = 0; x < x_end; ++x)
-// {
-//     for (int y = 0; y < y_end; ++y)
-//     {
-//         auto v_desc = descriptors[std::make_tuple(x, y)];
-//         char height = lines[y][x];
-//         if (height == 'S')
-//         {
-//             height = 'a';
-//             start = std::make_tuple(x, y);
-//             start_desc = v_desc;
-//             std::cerr << "S" << start_desc << std::endl;
-//         }
-//         if (height == 'E')
-//         {
-//             height = 'z';
-//             end = std::make_tuple(x, y);
-//             end_desc = v_desc;
-//             std::cerr << "E" << end_desc << std::endl;
-//         }
-
-//         // std::cout << "(" << x << ", " << y << ") is " << height << std::endl;
-
-//
-//     }
-// }
 
 int main(int argc, char *argv[])
 {
